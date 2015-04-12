1ffc45409f6ec95d0a1344bf61c98be8a285a28f
Muhammad Hijazi <irekasoft@gmail.com>
2015-04-13 06:18:23 +0800

    Initial Commit

---
+++ IRCustomUserInterfaceDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IRCustomUserInterfaceDemo
+//
+//  Created by Hijazi on 13/4/15.
+//  Copyright (c) 2015 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
